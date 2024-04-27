11e1dd35be3256aafc9289f8c9fbe958f71da760
Kauã da Silva de Mattos <kauasilvamattos0000@gmail.com>
2024-04-27 12:02:45 -0300

    ex008

---
+++ projects/aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 60.5
+console.log(`A velocidade do seu carro e ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log(`voce ultrapassou a velocidade permitida. Multado!`)
+
+}
+console.log(`Dirija sempre usando cinto de seguranca! `)
