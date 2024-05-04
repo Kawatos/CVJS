47b262c4949f26cacee4dd82c5f156894567a5f9
Kauã da Silva de Mattos <kauasilvamattos0000@gmail.com>
2024-05-04 13:29:59 -0300

    loop

--- projects/aula13/ex01/index.js
+++ projects/aula13/ex01/index.js
@@ -3,3 +3,8 @@ while (c < 6) {
     console.log('GG')
     c++
 }
+
+do {
+    console.log('GG2')
+    c++
+} while (c < 12)
---
+++ projects/aula14/exercios da aula/ambiente.js
@@ -0,0 +1,10 @@
+/* for (var c = 1; c < 10; c++) {
+    console.log('GG')
+}
+ */
+
+console.log('Vai comecar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('Fim.')
